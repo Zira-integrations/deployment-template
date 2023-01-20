9d7e9fa37c30db167f0a498cfec73519121f6836
gil-zira <gil.cohen@zira.us>
2023-01-20 14:16:53 -0800

    updated naming

--- serverless/resources/ses.cjs
+++ serverless/resources/ses.cjs
@@ -3,50 +3,49 @@ const extraResources = {
 }
 
 
-async function buildSesRules({ resolveVariable }) {
-    try {
-        const config = await resolveVariable('self:custom.context')
-        const stage = await resolveVariable('self:provider.stage');
-        const resources = config.reduce((acc, configItem, index) => {
-            if (!configItem.emailPrefix || !configItem.s3Prefix) return acc
-            const capitalizedLambdaName = configItem.adapter.charAt(0).toUpperCase() + configItem.adapter.slice(1)
-            const newResource = {
-                [capitalizedLambdaName + 'SESRule']: {
-                    "Type": "AWS::SES::ReceiptRule",
-                    "Properties": {
-                        "RuleSetName": "ruleSet",
-                        "Rule": {
-                            "Name": capitalizedLambdaName + "Rule" + (index + 1),
-                            "Enabled": true,
-                            "ScanEnabled": true,
-                            "Recipients": [
-                                configItem.emailPrefix + "@int.zira.us"
-                            ],
-                            "Actions": [
-                                {
-                                    "S3Action": {
-                                        "BucketName": `integrations-data-zira-${stage}`,
-                                        "ObjectKeyPrefix": configItem.s3Prefix
-                                    }
-                                }
-                            ]
-                        }
-
-                    }
+async function buildSesRules ({ resolveVariable }) {
+  try {
+    const config = await resolveVariable('self:custom.context')
+    const stage = await resolveVariable('self:provider.stage')
+    const resources = config.reduce((acc, configItem, index) => {
+      if (!configItem.emailPrefix || !configItem.s3Prefix) return acc
+      const capitalizedLambdaName =
+        configItem.adapter.charAt(0).toUpperCase() + configItem.adapter.slice(1)
+      const sesName = configItem.emailPrefix.replace(/[-.]/, '') + capitalizedLambdaName
+      const newResource = {
+        [sesName + 'SESRule']: {
+          Type: 'AWS::SES::ReceiptRule',
+          Properties: {
+            RuleSetName: 'ruleSet',
+            Rule: {
+              Name: sesName + 'Rule' + (index + 1),
+              Enabled: true,
+              ScanEnabled: true,
+              Recipients: [configItem.emailPrefix + '@int.zira.us'],
+              Actions: [
+                {
+                  S3Action: {
+                    BucketName: `integrations-data-zira-${stage}`,
+                    ObjectKeyPrefix: configItem.s3Prefix
+                  }
                 }
+              ]
             }
-            return { ...acc, ...newResource }
-        }, {})
-
-        return {
-            Resources: {
-                ...resources,
-                ...extraResources
-            }
+          }
         }
-    } catch (err) {
-        console.error(err)
+      }
+      return { ...acc, ...newResource }
+    }, {})
+
+    return {
+      Resources: {
+        ...resources,
+        ...extraResources
+      }
     }
+  } catch (err) {
+    console.error(err)
+  }
 }
 
 module.exports = buildSesRules
