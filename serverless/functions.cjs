7fd1400c54db1282fb40b71105a87ed9e7dbaeb6
gil-zira <gil.cohen@zira.us>
2023-01-24 14:50:29 -0800

    comments for units

--- serverless/functions.cjs
+++ serverless/functions.cjs
@@ -6,8 +6,8 @@ async function buildFunctions({ resolveVariable }) {
             const newFunction = {
                 [configItem.adapter]: {
                     "handler": `adapters/${configItem.adapter}.handler`,
-                    "timeout": 600,
-                    "memorySize": 512
+                    "timeout": 600, // in seconds
+                    "memorySize": 512 // in MB
                 }
             }
             return { ...acc, ...newFunction }
